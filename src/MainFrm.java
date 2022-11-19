e59efa26cbaf815f4ba0c247684849f3228dfa8e
Lakos Alex <lakosalexx@gmail.com>
2022-11-19 11:03:48 +0100

    fully working editUser and GUI

--- src/MainFrm.java
+++ src/MainFrm.java
@@ -45,14 +45,16 @@ public class MainFrm extends JFrame {
 
     public MainFrm(){
 
+        bttnSave.setEnabled(false);
+        bttnCancel.setEnabled(false);
+        txtPn.setEditable(false);
         fillTxtPnCmbBx();
         bttnRegistration.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 char[] password = psswrdFildPassword.getPassword();
-                int role = cmbBxRole.getSelectedIndex();
                 db = new DB();
-                db.insertUserIntoDB(txtFldUsername.getText(),password, txtFldFullname.getText(), txtFldAddress.getText(), role);
+                db.insertUserIntoDB(txtFldUsername.getText(),password, txtFldFullname.getText(), txtFldAddress.getText(), cmbBxRole.getSelectedIndex());
                 resetControls();
                 fillTxtPnCmbBx();
             }
@@ -69,12 +71,30 @@ public class MainFrm extends JFrame {
         bttnEdit.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                setComponentEnabledState(false, false, false, false, false, false, true, true, false );
                 db = new DB();
                 user = db.getUser(cmbBxUsers.getSelectedItem().toString());
                 txtFldUsername.setText(user.getUsername());
                 txtFldAddress.setText(user.getAddress());
                 txtFldFullname.setText(user.getFullName());
                 cmbBxRole.setSelectedIndex(Integer.parseInt(user.getRole()) - 1);
+            }
+        });
+        bttnCancel.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                setComponentEnabledState(true, true, true, true, true, true, false, false, true);
+                resetControls();
+            }
+        });
+        bttnSave.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                db = new DB();
+                db.editUser(txtFldUsername.getText(), txtFldFullname.getText(), txtFldAddress.getText(), cmbBxRole.getSelectedIndex());
+                setComponentEnabledState(true, true, true, true, true, true, false, false, true);
+                resetControls();
+                fillTxtPnCmbBx();
             }
         });
     }
@@ -86,6 +106,19 @@ public class MainFrm extends JFrame {
         txtFldFullname.setText("");
         txtFldAddress.setText("");
         cmbBxRole.setSelectedIndex(-1);
+    }
+
+    public void setComponentEnabledState(boolean bttnDeleteEnabled, boolean bttnEditEnabled, boolean bttnWriteEnabled, boolean bttnRegistrationEnabled, boolean cmbBxUsersEnabled, boolean psswrdFildPasswordEnabled, boolean bttnSaveEnabled, boolean bttnCancelEnabled, boolean txtFldUsernameEnabled){
+
+        bttnDelete.setEnabled(bttnDeleteEnabled);
+        bttnEdit.setEnabled(bttnEditEnabled);
+        bttnWrite.setEnabled(bttnWriteEnabled);
+        bttnRegistration.setEnabled(bttnRegistrationEnabled);
+        cmbBxUsers.setEnabled(cmbBxUsersEnabled);
+        psswrdFildPassword.setEnabled(psswrdFildPasswordEnabled);
+        bttnSave.setEnabled(bttnSaveEnabled);
+        bttnCancel.setEnabled(bttnCancelEnabled);
+        txtFldUsername.setEnabled(txtFldUsernameEnabled);
     }
 
     public void fillTxtPnCmbBx(){
